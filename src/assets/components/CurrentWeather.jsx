27683570196f8773b17601e6d41e670d0f827b30
NRS\nrsha <nrshakib007@gmail.com>
2024-04-01 23:11:51 +0600

    Updated BG Color based on Weather Condition

--- src/assets/components/CurrentWeather.jsx
+++ src/assets/components/CurrentWeather.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const CurrentWeather = ({ data, unit }) => {
   const { main, weather, name, sys } = data;
-  // console.log(main);
+  console.log(weather);
   const [temperature, setTemperature] = useState({
     celsius: main.temp,
     fahrenheit: (main.temp * 9) / 5 + 32,
@@ -25,8 +25,29 @@ const CurrentWeather = ({ data, unit }) => {
   const currentDate = new Date().toLocaleDateString();
   const currentTime = new Date().toLocaleTimeString();
 
+  // Function to determine background color based on weather description
+  const getBackgroundColor = (description) => {
+    if (description.includes("clear")) {
+      return "bg-blue-300"; // Define your own class for clear weather
+    } else if (description.includes("clouds")) {
+      return "bg-gray-800 text-white"; // Define your own class for cloudy weather
+    } else if (
+      description.includes("rain") ||
+      description.includes("drizzle")
+    ) {
+      return "bg-gray-300"; // Define your own class for rainy weather
+    } else if (description.includes("haze")) {
+      return "bg-gray-500"; // Define your own class for snowy weather
+    } else {
+      return "bg-black text-white"; // Default background color
+    }
+  };
   return (
-    <div className="text-center bg-blue-300 p-5 transition-transform transform hover:scale-110">
+    <div
+      className={`text-center p-5 transition-transform transform hover:scale-110 ${getBackgroundColor(
+        weather[0].description
+      )}`}
+    >
       <h2 className="text-2xl font-bold">
         {name}, {sys.country}
       </h2>

95c1a37ef0bc8f3720a429edfd9764250ce6b465
NRS\nrsha <nrshakib007@gmail.com>
2024-04-01 15:05:56 +0600

    Fetched data using api

---
+++ src/assets/API/Api.jsx
@@ -0,0 +1,14 @@
+const API_KEY = import.meta.env.VITE_WEATHER_APP;
+// console.log(API_KEY);
+const API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const getCurrentWeatherData = async (location, unit) => {
+  const response = await fetch(
+    `${API_URL}/weather?q=${location}&units=${unit}&appid=${API_KEY}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch current weather data");
+  }
+  const data = await response.json();
+  return data;
+};
